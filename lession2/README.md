2992370f0e10dbac556d0e58876044dfbcfc8179
Billy Huang <camber@poi.science>
2020-06-16 22:12:08 +0800

    Lession 2

---
+++ lession2/README.md
@@ -0,0 +1,8 @@
+# 第二节课作业
+
+Spectacular Luulia-Uusam. 
+
+[led.c](led.c): 作业. 按要求改名为`led.c`, 原文件名`Spectacular Luulia-Uusam.ino`.
+[led.png](led.png): 作业. 按要求改名为`led.png`, 源文件名`Spectacular Luulia-Uusam.png`.
+[Sketch.ino](Sketch.ino): 非作业. 实验性源文件.
+[Spectacular Luulia-Uusam.brd](Spectacular%20Luulia-Uusam.brd): 非作业. 原始文件.
---
+++ lession2/Sketch.ino
@@ -0,0 +1,14 @@
+void setup() {
+    // put your setup code here, to run once:
+    for(int i=0; i<8;i++)
+        pinMode(i, OUTPUT)
+}
+void loop() {
+    // put your main code here, to run repeatedly:
+    for(int i=0; i<8; i++){
+        digitalWrite(i, HIGH);
+        delay(500);
+        digitalWrite(i, LOW);
+        delay(500);
+    }
+}
